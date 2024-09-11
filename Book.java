34ec9be5546e23e5057613253c4d0cf85f489fc4
Gleb <glebkabaev200307@gmail.com>
2024-09-11 23:16:34 +0300

    №9

--- Book.java
+++ Book.java
@@ -2,57 +2,20 @@ package com.is;
 
 
 
-public class Book {
-    private String title;
-    private String author;
-    private String genere;
+public class Book extends ShortBook {
+    
     private int quantity;
     private double depositAmount;
     private double rentalCost;
     //Constructor
     public Book(String title, String author, String genere, int quantity, double depositAmount, double rentalCost) {
-        this.title = title;
-        if(validateString(author)){
-            this.author = author;
-        } else {
-            throw new IllegalArgumentException("Author name is invalid.");
-        }
-        if(validateString(genere)){
-            this.genere = genere;
-        } else {
-            throw new IllegalArgumentException("Genre name is invalid.");
-        }
+        super(title, author, genere);
         this.quantity =validateNumberField(quantity);
         this.depositAmount =validateNumberField(depositAmount);
         this.rentalCost = validateNumberField(rentalCost);
     }
     //Getters and setters
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        if(validateString(author)){
-            this.author = author;
-        } else {
-            throw new IllegalArgumentException("Author name is invalid.");
-        }
-    }
-    public String getGenere() {
-        return genere;
-    }
-    public void setGenere(String genere) {
-        if(validateString(genere)){
-            this.genere = genere;
-        } else {
-            throw new IllegalArgumentException("Genre name is invalid.");
-        }
-    }
+    
     public int getQuantity() {
         return quantity;
     }
@@ -74,10 +37,8 @@ public class Book {
     //toString method
     @Override
     public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", genere='" + genere + '\'' +
+        
+        return super.toString()+
                 ", quantity=" + quantity +
                 ", depositAmount=" + depositAmount +
                 ", rentalCost=" + rentalCost +
@@ -85,9 +46,7 @@ public class Book {
     }
     public String shortInfo() {
         return 
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", genere='" + genere + '\'';
+        super.toString();
                 
     }
    
@@ -104,21 +63,6 @@ public class Book {
         }
         return number;
     }
-    public static boolean validateString(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (!isValidCharacter(ch)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private static boolean isValidCharacter(char ch) {
-        return (ch >= 'A' && ch <= 'Z') || // Латинские заглавные буквы
-               (ch >= 'a' && ch <= 'z') || // Латинские строчные буквы
-               (ch >= 'А' && ch <= 'Я') || // Кириллические заглавные буквы
-               (ch >= 'а' && ch <= 'я') || // Кириллические строчные буквы
-               (ch == 'ё' || ch == 'Ё') || // Дополнительные кириллические буквы
-               (ch == ' ');                // Пробел
-    }
+   
+    
 }
